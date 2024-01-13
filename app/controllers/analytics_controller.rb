f23c0fe69323d0fdf44d4cb6edcccc5556c452a0
karvel-code <velgithinji@gmail.com>
2024-01-13 19:45:27 +0300

    Add list of queries

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,5 +2,6 @@ class AnalyticsController < ApplicationController
   def index
     @search_data = Article.includes(:analytic).group('articles.title').sum('analytics.no_of_searches')
     @view_data = Article.includes(:analytic).group('articles.title').sum('analytics.no_of_views')
+    @searched_terms = Search.order(no_of_times_searched: :desc)
   end
 end
