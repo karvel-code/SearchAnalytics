7a1810f3360ec89821899e828be8c3b50d5e2573
karvel-code <velgithinji@gmail.com>
2024-01-13 18:04:37 +0300

    Add article and search analytics functionalities

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,16 +1,11 @@
 require 'fuzzystringmatch'
 
 class Search < ApplicationRecord
-  # Relationships
+  # Associations
   belongs_to :user
 
   # Validations
-  validates :query, presence: true
-
-  # Callbacks
-  # before_create :check_and_update_existing_search
-  # before_save :update_article_analytics
-
+  validates :query, presence: true, uniqueness: true
 
   # def self.save_search(query, user)
   #   last_search = user.searches.last
@@ -31,7 +26,6 @@ class Search < ApplicationRecord
       Search.create(query:, user: user)
     else
       last_search.update(query:)
-      increment_search_count(query)
     end
   end
 
@@ -39,6 +33,8 @@ class Search < ApplicationRecord
     return false if query.blank? || query.length < 3
     return true if last_search.nil? || !query.include?(last_search.query)
 
+    increment_search_count(query)
+    update_article_analytics(query)
     last_search.query.length < query.length
   end
 
@@ -48,31 +44,19 @@ class Search < ApplicationRecord
     distance > 0.8
   end
 
-  def increment_search_count(query)
+  def self.increment_search_count(query)
     search = Search.find_by(query: query)
     if search
-      search.update_columns(no_of_searches: + 1)
-    else
-      create(query: query, no_of_searches: 1)
+      no_of_times_searched = search.no_of_times_searched
+      no_of_times_searched = no_of_times_searched += 1
+      search.update(no_of_times_searched: no_of_times_searched)
     end
   end
 
-  # def check_and_update_existing_search(query)
-  #   existing_search = find_similar_search(query)
-  #   if existing_search
-  #     existing_search.increment!(:no_of_searches)
-  #     throw(:abort)
-  #   end
-  # end
-
-  # def find_similar_search(query)
-  #   Search.find_by("query ILIKE ?", "%#{query}%")
-  # end
-
-  def update_article_analytics
-    article = Article.find_by(title: search.query)
-    if article.exists?
-      if article.analytic.exists?
+  def self.update_article_analytics(query)
+    article = Article.find_by(title: query)
+    if article.present?
+      if article.analytic.present?
         article.analytic.increment!(:no_of_searches)
       else
         article.create_analytic(no_of_searches: 1)
